f4b78fac2e46b1ee25dda80028d0b1cfa291265f
chijekwu <chijekwu1234@gmail.com>
2020-04-22 12:29:05 -0400

    new rps

--- PseudocodeForRPS.cs
+++ PseudocodeForRPS.cs
@@ -1,58 +1,140 @@
-/*
-    Rock Paper Shotgun 
-    
-    A small game that will let 2 players play a game of RPS. Choices will be chosen randomly for each player.
-    Best 2 out 3 wins!
-    
-    @Authors - Kingsley Ononachi (chijekwu), David Sawyer (d-sawyer), Michael Walker (Mohcka)
-*/
-            
-using System;  
+using System;
+using System.Collections.Generic;
 
 namespace rps
 {
-    class Game
+    class Program
     {
-        // Instantiate players
-        //Player player1;
-        //Player player2;
-        
-        //create variable to count number of rounds
-        //int rounds;
+
+
+
         static void Main(string[] args)
         {
-            //assign players with scores of 0
-            
-            // ask for input of player1 and player 2's  name and store it their respective property
 
-            // create an array of strings that will hold the values of "Rock", "Paper", "Scissors" to compare
-            // and decide winner
 
-            // start while loop that will keep running until either player1 or player2's score value is => 2
-            
-            // create temp variables for player1 and player2 that will store the hand they drew (Rock, Paper, Scissor)
-            
-            // Randomly choose the hand each player drew and store into temp their temp variable
+            Game rps = new Game();
+
+            rps.StartGame();
+
+
+
 
             // compare the player's value, check if there's a tie, if so give no score, increment round, restart the loop
             // else give point to winner and store point to Player's score property
-            
+
             // print out result of the players, which hand they drew, which player won or state that there's a tie and what the
             // current round is
-            
+
             // end of loop logic, at this point a player should have 2 points
 
 
-            // finally, print the winner, their W-L ratio and their total amount of ties
-            
+
             // endgame
         }
     }
-    
+
+    class Game
+    {
+        //Instantiate players
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        //create variable to count number of rounds
+        int rounds;
+        int ties;
+
+        // Starts the game.  Sets rounds and scores to 0.  Asks for players name.
+        public void StartGame()
+        {
+            //assign players with scores of 0 and start at round 0
+            rounds = 1;
+            ties = 0;
+            player1.score = 0;
+            player2.score = 0;
+
+            // ask for input of player1 and player 2's  name and store it their respective property
+            Console.WriteLine("Enter first player name:");
+            player1.playerName = Console.ReadLine();
+
+            Console.WriteLine("Enter second player name:");
+            player2.playerName = Console.ReadLine();
+
+
+            Console.WriteLine($"Player1's name is:  {player1.playerName}");
+            Console.WriteLine($"Player2's name is:  {player2.playerName}");
+
+
+            // start while loop that will keep running until either player1 or player2's score value is => 2
+            while (player1.score < 2 && player2.score < 2)
+            {
+                // Randomly choose the hand each player drew and store into temp their temp variable
+                player1.hand = ChooseHand();
+                player2.hand = ChooseHand();
+
+                // print out result of the players, which hand they drew, which player won or state that there's a tie and what the
+                // current round is
+                Console.WriteLine($"Starting Round {rounds}");
+                Console.WriteLine($"Player {player1.playerName} choose  {player1.hand}");
+                Console.WriteLine($"Player {player2.playerName} choose  {player2.hand}");
+
+                // compare
+                compare();
+
+                rounds++;
+            }
+
+            // finally, print the winner, their W-L ratio and their total amount of ties
+            if (player1.score > player2.score)
+            {
+                Console.WriteLine($"{player1.playerName} wins {player1.score}-{player2.score} with {ties} ties");
+            }
+            else
+            {
+                Console.WriteLine($"{player2.playerName} wins {player2.score}-{player1.score} with {ties} ties");
+            }
+
+
+        }
+
+        // Returns a random hand
+        public string ChooseHand()
+        {
+            // create an array of strings that will hold the values of "Rock", "Paper", "Scissors" to compare
+            // and decide winner
+            var random = new Random();
+            var list = new List<string> { "rock", "paper", "scissors" };
+            int index = random.Next(list.Count);
+            return list[index];
+        }
+
+        // compare player hands and deterine winner or tie
+        public void compare()
+        {
+            if (player1.hand == player2.hand)
+            {
+                ties++;
+            }
+            else if ((player1.hand == "rock" && player2.hand == "scissors") || (player1.hand == "scissors" && player2.hand == "paper") || (player1.hand == "paper" && player2.hand == "rock"))
+            {
+                player1.score++;
+                Console.WriteLine($"Player {player1.playerName} wins.");
+            }
+            else
+            {
+                player2.score++;
+                Console.WriteLine($"Player {player2.playerName} wins.");
+            }
+        }
+    }
+
     class Player
     {
         // define properties for player
         // player name
+        public string playerName;
         // player score
+        public int score;
+        // players hand
+        public string hand = "EMPTY";
     }
 }
